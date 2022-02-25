c4843be16ee38d1f30772272a23b31b114f6685f
Felipe Moreno Borges <Felipe_Moreno_Borges@gap.com>
2022-02-25 20:17:05 -0300

    dependencies: update dependencies version and adding navigation library

--- app/src/main/java/br/com/devcapu/beehealthy/ui/activity/RegisterActivity.kt
+++ app/src/main/java/br/com/devcapu/beehealthy/ui/activity/RegisterActivity.kt
@@ -8,55 +8,108 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.devcapu.beehealthy.R
+import br.com.devcapu.beehealthy.data.database.dataSource.HealthResultDataSource
+import br.com.devcapu.beehealthy.data.database.dataSource.PatientDataSource
 import br.com.devcapu.beehealthy.ui.component.FormWithBeeHealthIdentity
+import br.com.devcapu.beehealthy.ui.component.OutlineDropDownMenu
 import br.com.devcapu.beehealthy.ui.component.PasswordTrailingIcon
 import br.com.devcapu.beehealthy.ui.viewModel.RegisterViewModel
+import br.com.devcapu.domain.model.BiologicalGender
 
 class RegisterActivity : ComponentActivity() {
 
-    private val viewModel: RegisterViewModel by viewModels()
+    private val viewModel: RegisterViewModel by viewModels {
+        val patientDataSource = PatientDataSource(this)
+        val healthResultDataSource = HealthResultDataSource(this)
+        RegisterViewModel.Factory(
+            patientDataSource = patientDataSource,
+            healthResultDataSource = healthResultDataSource
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.goToNextStep.observe(this) {
-            if (it) {
-                val intent = HealthRegisterActivity.getIntent(this)
-                intent.putExtra("PATIENT_EMAIL", viewModel.email)
-                startActivity(intent)
-            }
-        }
 
         setContent {
-            var showPassword by remember { mutableStateOf(false) }
-            val passwordVisualizationMode = if (showPassword) {
-                VisualTransformation.None
-            } else {
-                PasswordVisualTransformation()
-            }
+            val navController = rememberNavController()
 
-            RegisterContent(
-                email = viewModel.email,
-                onEmailChange = { viewModel.email = it },
-                password = viewModel.password,
-                onPasswordChange = { viewModel.password = it },
-                passwordConfirmation = viewModel.passwordConfirmation,
-                onPasswordConfirmationChange = { viewModel.passwordConfirmation = it },
-                showPassword = showPassword,
-                passwordVisualizationMode = passwordVisualizationMode,
-                onChangePasswordVisualizationMode = { showPassword = !showPassword },
-                onClickRegisterButton = { viewModel.register() },
-                onClickAlreadyHasAnAccount = { goToLoginActivity() }
-            )
+            NavHost(navController = navController, startDestination = "emailAndPassword") {
+                composable("emailAndPassword") {
+
+                    var showPassword by remember { mutableStateOf(false) }
+                    val passwordVisualizationMode = if (showPassword) {
+                        VisualTransformation.None
+                    } else {
+                        PasswordVisualTransformation()
+                    }
+
+                    RegisterContent(
+                        email = viewModel.email,
+                        onEmailChange = { viewModel.email = it },
+                        password = viewModel.password,
+                        onPasswordChange = { viewModel.password = it },
+                        passwordConfirmation = viewModel.passwordConfirmation,
+                        onPasswordConfirmationChange = { viewModel.passwordConfirmation = it },
+                        showPassword = showPassword,
+                        passwordVisualizationMode = passwordVisualizationMode,
+                        onChangePasswordVisualizationMode = { showPassword = !showPassword },
+                        onClickRegisterButton = {
+                            viewModel.initSignUp()
+                                .observe(this@RegisterActivity) { userHasBeenCreated ->
+                                    if (userHasBeenCreated) {
+                                        navController.navigate("initialInfoForm")
+                                    }
+                                }
+                        },
+                        onClickAlreadyHasAnAccount = { goToLoginActivity() }
+                    )
+                }
+                composable("initialInfoForm") {
+                    val genders = listOf("Masculino", "Feminino")
+                    val objectives = listOf("Perder", "Definir", "Manter")
+                    val activityLevel = listOf("Baixo", "Médio", "Alto")
+
+                    HealthRegisterContent(
+                        name = viewModel.name,
+                        onNameChange = { viewModel.name = it },
+                        age = viewModel.age,
+                        onAgeChange = { viewModel.age = it },
+                        height = viewModel.height,
+                        onHeightChange = { viewModel.height = it },
+                        weight = viewModel.weight,
+                        onWeightChange = { viewModel.weight = it },
+                        biologicalGenders = genders,
+                        onBiologicalGenderChange = {
+                            if (genders[it] == getString(R.string.male_option)) {
+                                viewModel.biologicGender = BiologicalGender.MALE.toString()
+                            } else {
+                                viewModel.biologicGender = BiologicalGender.FEMALE.toString()
+                            }
+                        },
+                        objectives = objectives,
+                        onObjectiveChange = { viewModel.objective = objectives[it] },
+                        activitiesLevel = activityLevel,
+                        onActivityLevelChange = {
+                            viewModel.activityLevel = activityLevel[it]
+                        },
+                        finishSignUp = { viewModel.finishSignUp() }
+                    )
+                }
+            }
         }
     }
 
@@ -95,13 +148,15 @@ fun RegisterContent(
 
         OutlinedTextField(
             value = password,
-            onValueChange = onPasswordChange ,
+            onValueChange = onPasswordChange,
             placeholder = { Text(text = "Senha") },
             visualTransformation = passwordVisualizationMode,
             trailingIcon = {
                 PasswordTrailingIcon(showPassword = showPassword) { onChangePasswordVisualizationMode() }
             },
-            modifier = Modifier.padding(bottom = 16.dp).fillMaxWidth()
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth()
         )
 
         OutlinedTextField(
@@ -109,15 +164,128 @@ fun RegisterContent(
             onValueChange = onPasswordConfirmationChange,
             placeholder = { Text(text = "Confirmar senha") },
             visualTransformation = passwordVisualizationMode,
-            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
         )
 
         Button(
             onClick = onClickRegisterButton,
-            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
         ) { Text(text = "Entrar") }
 
         TextButton(onClick = onClickAlreadyHasAnAccount) { Text(text = "Já tenho uma conta") }
+    }
+}
+
+@Composable
+fun HealthRegisterContent(
+    name: String,
+    onNameChange: (String) -> Unit,
+    age: String,
+    onAgeChange: (String) -> Unit,
+    height: String,
+    onHeightChange: (String) -> Unit,
+    weight: String,
+    onWeightChange: (String) -> Unit,
+    biologicalGenders: List<String>,
+    onBiologicalGenderChange: (Int) -> Unit,
+    objectives: List<String>,
+    onObjectiveChange: (Int) -> Unit,
+    activitiesLevel: List<String>,
+    onActivityLevelChange: (Int) -> Unit,
+    finishSignUp: () -> Unit,
+) {
+    FormWithBeeHealthIdentity(modifier = Modifier.verticalScroll(state = rememberScrollState())) {
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            placeholder = { Text(text = "Name") },
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = age,
+            onValueChange = onAgeChange,
+            placeholder = { Text(text = "Age") },
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = height,
+            onValueChange = onHeightChange,
+            placeholder = { Text(text = "Altura") },
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = weight,
+            onValueChange = onWeightChange,
+            placeholder = { Text(text = "peso") },
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth()
+        )
+
+        OutlineDropDownMenu(
+            values = biologicalGenders,
+            selectedIndex = 0,
+            onChange = onBiologicalGenderChange,
+            label = {
+                Text(
+                    text = "Sexo biológico",
+                    color = MaterialTheme.colors.onSurface.copy(ContentAlpha.medium)
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+        OutlineDropDownMenu(
+            values = objectives,
+            selectedIndex = 0,
+            onChange = onObjectiveChange,
+            label = {
+                Text(
+                    text = "Objetivo",
+                    color = MaterialTheme.colors.onSurface.copy(ContentAlpha.medium)
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+        OutlineDropDownMenu(
+            values = activitiesLevel,
+            selectedIndex = 0,
+            onChange = onActivityLevelChange,
+            label = {
+                Text(
+                    text = "Nível de atividade",
+                    color = MaterialTheme.colors.onSurface.copy(ContentAlpha.medium)
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 32.dp)
+        )
+
+        Button(
+            onClick = finishSignUp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
+        ) { Text(text = "Pronto!") }
     }
 }
 
@@ -126,7 +294,7 @@ fun RegisterContent(
 fun RegisterScreenPreview() {
     RegisterContent(
         email = "",
-        password =  "",
+        password = "",
         passwordConfirmation = "",
         onEmailChange = {},
         onPasswordChange = {},
--- app/src/main/java/br/com/devcapu/beehealthy/ui/viewModel/RegisterViewModel.kt
+++ app/src/main/java/br/com/devcapu/beehealthy/ui/viewModel/RegisterViewModel.kt
@@ -1,43 +1,90 @@
 package br.com.devcapu.beehealthy.ui.viewModel
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import br.com.devcapu.beehealthy.data.database.dataSource.HealthResultDataSource
+import br.com.devcapu.beehealthy.data.database.dataSource.PatientDataSource
+import br.com.devcapu.domain.model.BiologicalGender
+import br.com.devcapu.domain.model.Patient
+import br.com.devcapu.domain.repository.HealthRepository
+import br.com.devcapu.domain.repository.PatientRepository
+import br.com.devcapu.domain.useCase.SavePatient
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class RegisterViewModel : ViewModel() {
+class RegisterViewModel(
+    private val patientDataSource: PatientDataSource,
+    private val healthResultDataSource: HealthResultDataSource,
+) : ViewModel() {
 
     var email by mutableStateOf("")
     var password by mutableStateOf("")
     var passwordConfirmation by mutableStateOf("")
+    var name by mutableStateOf("")
+    var age by mutableStateOf("")
+    var weight by mutableStateOf("")
+    var height by mutableStateOf("")
+    var biologicGender by mutableStateOf("MALE")
+    var activityLevel by mutableStateOf("")
+    var objective by mutableStateOf("")
 
-    private val passwordsAreTheSame
-        get() = password == passwordConfirmation
+    private val passwordsAreTheSame = password == passwordConfirmation
 
     private val _goToNextStep = MutableLiveData(false)
     val goToNextStep: LiveData<Boolean> = _goToNextStep
 
-    fun register() {
+    private val _userCreated = MutableLiveData(false)
+    val userCreated: LiveData<Boolean> = _userCreated
+
+    fun initSignUp(): LiveData<Boolean> {
         val auth = Firebase.auth
         if (passwordsAreTheSame) {
-            val createUserWithEmailAndPassword = auth.createUserWithEmailAndPassword(
-                email, password
-            )
-            createUserWithEmailAndPassword.addOnSuccessListener {
-                _goToNextStep.value = true
-            }
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    _userCreated.value = true
+                }.addOnFailureListener {
+                    _userCreated.value = false
+                }
+        }
+        return userCreated
+    }
 
-            createUserWithEmailAndPassword.addOnFailureListener {
-                Log.e("RegisterViewModel",
-                    "Register: ${it.message}" +
-                        "\n email: $email" +
-                        "\n password: $password")
-            }
+    fun finishSignUp() {
+        val patient = Patient(
+            name = name,
+            email = email,
+            age = age.toInt(),
+            weight = weight.toFloat(),
+            height = height.toFloat(),
+            biologicGender = BiologicalGender.valueOf(biologicGender.uppercase()),
+            activityLevel = activityLevel,
+            objective = objective
+        )
+
+        val patientRepository = PatientRepository(patientDataSource)
+        val healthRepository = HealthRepository(healthResultDataSource)
+        val savePatient = SavePatient(
+            patientRepository = patientRepository,
+            healthRepository = healthRepository
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            savePatient(patient = patient)
+        }
+        _userCreated.value = true
+    }
+
+    class Factory(
+        private val patientDataSource: PatientDataSource,
+        private val healthResultDataSource: HealthResultDataSource,
+    ) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return RegisterViewModel(patientDataSource, healthResultDataSource) as T
         }
     }
 }
