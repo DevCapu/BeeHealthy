e19e729f9a21130fd8646eb97477bda9d7ddbcb4
Felipe Moreno Borges <felipe.b2014@gmail.com>
2022-12-22 23:05:32 -0300

    refactor: add card preview and card uses theme now

--- app/src/main/java/br/com/devcapu/beehealthy/common/ui/component/card/Card.kt
+++ app/src/main/java/br/com/devcapu/beehealthy/common/ui/component/card/Card.kt
@@ -1,14 +1,16 @@
 package br.com.devcapu.beehealthy.common.ui.component.card
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -17,7 +19,7 @@ fun PrimaryCard(
     body: @Composable () -> Unit
 ) {
     Card(
-        backgroundColor = Color.White,
+        backgroundColor = MaterialTheme.colors.background,
         contentColor = MaterialTheme.colors.onBackground,
         modifier = Modifier.padding(vertical = 8.dp)
     ) {
@@ -30,5 +32,17 @@ fun PrimaryCard(
             body()
         }
     }
+}
 
+@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+private fun PrimaryCardPreview() {
+    PrimaryCard(
+        header = { Text(text = "Eu sou o header") },
+        body = { Text(text = "Eu sou o body") }
+    )
 }
