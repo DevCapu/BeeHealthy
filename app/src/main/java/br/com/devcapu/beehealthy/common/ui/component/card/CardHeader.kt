a3ba562ba31d6d09f2b49fae600cbc5df8815f70
Felipe Moreno Borges <felipe.b2014@gmail.com>
2022-12-22 23:08:49 -0300

    refactor: add CardHeader preview and card uses theme now

--- app/src/main/java/br/com/devcapu/beehealthy/common/ui/component/card/CardHeader.kt
+++ app/src/main/java/br/com/devcapu/beehealthy/common/ui/component/card/CardHeader.kt
@@ -1,5 +1,7 @@
 package br.com.devcapu.beehealthy.common.ui.component.card
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,30 +17,42 @@ import androidx.compose.ui.unit.sp
 fun CardHeader(title: String, subtitle: String = "") {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth().background(MaterialTheme.colors.background)
     ) {
         Text(
             text = title,
             style = MaterialTheme.typography.h3,
             fontWeight = FontWeight.SemiBold,
-            fontSize = 18.sp
+            fontSize = 18.sp,
+            color = MaterialTheme.colors.onBackground
         )
         Text(
             text = subtitle,
             style = MaterialTheme.typography.h3,
             fontSize = 14.sp,
             fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colors.onBackground
         )
     }
 }
 
 @Preview(name = "With title and subtitle", showBackground = true)
+@Preview(
+    name = "With title and subtitle",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Composable
 fun CardHeaderWithTitleAndSubtilePreview() {
     CardHeader(title = "Refeições", subtitle = "3 de 5")
 }
 
 @Preview(name = "With title", showBackground = true)
+@Preview(
+    name = "With title",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Composable
 fun CardHeaderWithTitlePreview() {
     CardHeader(title = "Refeições")
